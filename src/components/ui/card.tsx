1529efa3234ed55828597c8ac0e9ff8fe4fb8131
Matias De Francisco <capucha08@gmail.com>
2025-06-20 10:04:06 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="rounde

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -10,7 +10,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm overflow-hidden", // overflow-hidden is important
+      "rounded-lg border bg-card text-card-foreground shadow-sm", // overflow-hidden removed
       className
     )}
     {...props}
