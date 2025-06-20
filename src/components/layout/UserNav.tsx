afd33a760457914ba79528dacea74d2cb292527f
Matias De Francisco <capucha08@gmail.com>
2025-06-20 02:03:40 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/components/layout/UserNav.tsx
+++ src/components/layout/UserNav.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button"; // Corregido: Rfrom -> from
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
