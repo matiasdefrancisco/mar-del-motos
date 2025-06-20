fd424f999ff5006a9d6e97e39ef7e907f0ae16d4
Matias De Francisco <capucha08@gmail.com>
2025-06-20 02:02:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/UserNav.tsx
+++ src/components/layout/UserNav.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } Rfrom "@/components/ui/button";
+import { Button } from "@/components/ui/button"; // Corregido: Rfrom -> from
 import {
   DropdownMenu,
   DropdownMenuContent,
